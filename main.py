8290fc76755a854bca68cb7d291f172843d77ac8
santipe01 <130890911+santipe01@users.noreply.github.com>
2024-04-14 12:52:12 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+from scraper import EmbalsesScraper
+import pandas as pd
+import time
+
+df = pd.DataFrame()
+url = "https://www.embalses.net"
+output_file="embalses.csv"
+
+scraper = EmbalsesScraper(url, df)
+scraper.scrape()
+scraper.data2csv(output_file)
+
+print("Quieres que se actualice cada semana el dataset? (Si/No)")
+respuesta = input()
+if respuesta in ["Si", "si", "SI"]:
+  while True:
+    scraper.update_scrape()
+    print("Quieres exportar el dataset actualizado? (Si/No)")
+    respuesta2 = input()
+      if respuesta2 in ["Si", "si", "SI"]:
+        scraper.data2csv(output_file)
+    time.sleep(604800)
