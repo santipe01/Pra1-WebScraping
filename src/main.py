e2f4e8eb4ea2e2cccabe9e8ecf07a93ac428901d
santipe01 <130890911+santipe01@users.noreply.github.com>
2024-04-16 20:21:17 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,21 +1,19 @@
 from scraper import EmbalsesScraperSelenium
 import pandas as pd
-import time
 
 df = pd.DataFrame()
 output_file="embalses.csv"
 
 scraper = EmbalsesScraperSelenium(df)
-scraper.scrape()
-scraper.data2csv(output_file)
 
-print("Quieres que se actualice cada semana el dataset? (Si/No)")
+print("Quieres hacer scrape o actualizar? (scrape/actualizar)")
 respuesta = input()
-if respuesta in ["Si", "si", "SI"]:
-  while True:
-    scraper.update_scrape()
-    print("Quieres exportar el dataset actualizado? (Si/No)")
-    respuesta2 = input()
-      if respuesta2 in ["Si", "si", "SI"]:
-        scraper.data2csv(output_file)
-    time.sleep(604800)
+if respuesta == "actualizar":
+  scraper.update_scrape(df)
+  print("Quieres exportar el dataset actualizado? (Si/No)")
+  respuesta2 = input()
+    if respuesta2 in ["Si", "si", "SI"]:
+      scraper.data2csv(output_file)
+else:
+  scraper.scrape()
+  scraper.data2csv(output_file)
